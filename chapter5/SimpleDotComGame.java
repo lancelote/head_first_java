9827fb109d632cb238d1e55c6813eb570e407317
Pavel Karateev <karateev.pavel@ya.ru>
2017-01-24 21:48:58 +0300

    Chapter 5 - SimpleDotCom game
    GameHelper to handle user input
    SimpleDotComGame implementation

--- chapter5/SimpleDotComGame.java
+++ chapter5/SimpleDotComGame.java
@@ -2,32 +2,46 @@ public class SimpleDotComGame {
 
     public static void main(String[] args) {
         // Declare integer numOfGuesses variable to store number of player turns
+        int numOfGuesses = 0;
+
+        // Special class to receive user input
+        GameHelper helper = new GameHelper();
 
         // Create a SimpleDotCom object
+        SimpleDotCom theDotCom = new SimpleDotCom();
 
         // Calculate random position of the initial cell from 0 to 4
+        int randomNum = (int) (Math.random() * 5);
 
         // Create an int array with 3 items using the calculated random number increasing it by 1 and then 2
+        int[] locations = {randomNum, randomNum + 1, randomNum + 2};
 
         // Set location cells
+        theDotCom.setLocationCells(locations);
 
         // Declare bool isAlive variable to store if the dotCom is still alive
+        boolean isAlive = true;
 
         // Wile dotCom isAlive
+        while (isAlive) {
             // Get user input
-
-            // Check user input to be correct
+            String guess = helper.getUserInput("Inter the number");
 
             // Call checkYourself
+            String result = theDotCom.checkYourself(guess);
 
             // Increase numOfGuesses
+            numOfGuesses++;
 
             // Check if the dotCom is sink
-            // If it is:
+            if (result.equals("Sink")) {
+                // If it is:
                 // Set isAlive to false
+                isAlive = false;
 
                 // Print numOfGuesses
-
-        // Tell user how many guess he took
+                System.out.println("Yo used " + numOfGuesses + " tries!");
+            }
+        }
     }
 }
